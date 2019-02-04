bc4decee0b007e984fdbbcba33a07677550fef65
lyc313 <46698393+lyc313@users.noreply.github.com>
2019-02-04 20:01:30 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import View from './View';
+import ShoppingList from './ShoppingList';
+import App2 from './App2';
+import Page2 from "./Page2";
+import Game from "./Game";
+
+//ReactDOM.render(<App/>, document.getElementById('root'));
+//ReactDOM.render(<View/>, document.getElementById('root'));
+//ReactDOM.render(<ShoppingList/>, document.getElementById('root'));
+//ReactDOM.render(<App2/>, document.getElementById('root'));
+//ReactDOM.render(<Page2/>, document.getElementById('root'));
+
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
+
+
